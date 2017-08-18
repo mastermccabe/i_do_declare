17495c75e7cb0a3e0f1cd6e095161ae7a8a8bf70
McCabe <mccabe@wambl.com>
2017-08-18 14:13:41 -0700

    first JS push

---
+++ i_do_declare.js
@@ -0,0 +1,18 @@
+
+
+
+var num1 = -1;
+var num2 = -92332;
+var num3 = .123456;
+var num4 = -123.321
+console.log("num1 ="+num1 + "\n num2 =" + num2 + "\n num2 =" + num2 + "\n num3 =" +num3 + " \n num4=" +num4);
+
+var string1 = "string 1";
+var string2 = "mccabe's \n was \n here \n  |\n  |\n  v";
+var string3 = "string of numbers 123123123";
+
+console.log(string1 + "\n" +string2 + string3);
+
+var bool1 = false;
+var bool2 = true;
+console.log(bool1);
